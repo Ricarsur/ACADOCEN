b4f39f832ff3c6d23c449216f8a13cd1eba183c9
Ricardo José Suárez Ravelo <ricarsur17@outlook.com>
2022-10-13 17:35:46 -0500

    Date in the utils for build the schedule

--- lib/UI/widgets/combo_box.dart
+++ lib/UI/widgets/combo_box.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Combobox extends StatefulWidget {
+  const Combobox({super.key});
+
+  @override
+  State<Combobox> createState() => _ComboboxState();
+}
+
+class _ComboboxState extends State<Combobox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
