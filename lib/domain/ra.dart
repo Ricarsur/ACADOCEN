e4b96a19567e94de45d7599c28e60c304fd124b9
DavidRaveloU <dravelo@unicesar.edu.co>
2022-10-18 20:44:38 -0700

    Calendar library customization

---
+++ lib/domain/ra.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ra extends StatefulWidget {
+  const Ra({super.key});
+
+  @override
+  State<Ra> createState() => _RaState();
+}
+
+class _RaState extends State<Ra> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
