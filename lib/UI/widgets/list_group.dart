dda124678bbd9b5023e80731bb2e897615c2086b
Augusto0414 <augustopelis4@gmail.com>
2022-10-20 16:16:25 -0500

    creating widget list_group

---
+++ lib/UI/widgets/list_group.dart
@@ -0,0 +1,54 @@
+import 'package:acadocen/misc/colors.dart';
+import 'package:flutter/material.dart';
+
+class ListGroup extends StatelessWidget {
+  final String name;
+  final String numberGroup;
+  final VoidCallback onPressed;
+
+  const ListGroup(
+      {super.key,
+      required this.name,
+      required this.numberGroup,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(children: [
+        Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              ColorsApp.gradiant1,
+              ColorsApp.gradiant2,
+            ]),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          child: ListTile(
+            leading: const Icon(Icons.group, color: Colors.white),
+            title: Text(name,
+                style: const TextStyle(fontSize: 20, color: Colors.white)),
+            subtitle: Text(numberGroup,
+                style: const TextStyle(fontSize: 15, color: Colors.white)),
+            trailing: IconButton(
+              icon: const Icon(Icons.arrow_forward_ios, color: Colors.white),
+              onPressed: () {
+                onPressed();
+              },
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
