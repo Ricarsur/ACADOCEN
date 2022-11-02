19e161e4f8ecba24ac8b83e82f00e105f42c8954
DavidRaveloU <dravelo@unicesar.edu.co>
2022-11-02 03:24:10 -0500

    fixed file name

--- lib/UI/widgets/nav_bar.dart
+++ lib/UI/widgets/nav_bar.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:acadocen/UI/pages/new%20schedule.dart';
+import 'package:acadocen/UI/pages/new_schedule.dart';
 import 'package:acadocen/UI/pages/new_group.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
