6627927afb3837a529916e074c4a0af426eb12f2
Augusto0414 <augustopelis4@gmail.com>
2022-11-30 18:11:45 -0500

    AttendanceList

---
+++ lib/UI/pages/asistencia/attendance.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AttendanceList extends StatefulWidget {
+  const AttendanceList({super.key});
+
+  @override
+  State<AttendanceList> createState() => _AttendanceListState();
+}
+
+class _AttendanceListState extends State<AttendanceList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
